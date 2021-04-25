6ad47071458cacff2da938d9055129e286a97aef
bhushan laware <bhushanlaware@gmail.com>
2021-04-25 23:02:43 +0530

    issue

--- src/client/components/layouts/Drawer/components/ResponsiveDrawer.tsx
+++ src/client/components/layouts/Drawer/components/ResponsiveDrawer.tsx
@@ -62,6 +62,15 @@ const useStyles = makeStyles((theme: Theme) =>
       justifyContent: "flex-start",
       alignItems: "center",
     },
+    toolbar: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "flex-end",
+      padding: theme.spacing(0, 1),
+      // necessary for content to be below app bar
+      color: theme.palette.text.primary,
+      ...theme.mixins.toolbar,
+    },
   })
 );
 
